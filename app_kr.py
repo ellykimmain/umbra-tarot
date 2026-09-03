e1ffb215b31b7aab435721094fcab3871f1859f8
ellykimmain <ellykimmain@gmail.com>
2026-09-03 19:34:52 +0900

    Update app_kr.py

--- app_kr.py
+++ app_kr.py
@@ -369,7 +369,7 @@ current_product = PRODUCTS[selected_product_id]
 MAJOR_ARCANA = [
     "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
     "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
-    "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance",
+    "Wheel of Fortune", "Justice", "The Hanged Man", "The Death", "Temperance",
     "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
     "Judgement", "The World"
 ]
